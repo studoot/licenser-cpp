b68a0eaf6114962ce5a11b1e8d5f35f77c98e9a4
Stuart Dootson <stuart.dootson@gmail.com>
2019-07-29 15:00:48 +0100

    Abstracted overload helper

---
+++ src/overloaded.hpp
@@ -0,0 +1,7 @@
+#ifndef OVERLOADED_HPP
+#define OVERLOADED_HPP
+
+template <class... Ts> struct overloaded : Ts... { using Ts::operator()...; };
+template <class... Ts> overloaded(Ts...)->overloaded<Ts...>;
+
+#endif /* OVERLOADED_HPP */
